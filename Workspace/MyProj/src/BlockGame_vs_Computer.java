71d1e219e96aadd0a4d75b0f6b25e76d9d565ff5
Freeze777 <freezefrancis@gmail.com>
2016-05-08 01:05:04 +0530

    euler few more

---
+++ Workspace/MyProj/src/BlockGame_vs_Computer.java
@@ -0,0 +1,69 @@
+import java.io.*;
+
+public class BlockGame_vs_Computer {
+
+	public static String[] amount_value(int input1, String[] input2) {
+		int[][] arr = new int[input1][input1];
+		int[][] res = new int[input1][input1];
+		for (int i = 0; i < input2.length; i++) {
+			String[] str_arr = input2[i].split("#");
+			for (int j = 0; j < str_arr.length; j++) {
+				arr[i][j] = Integer.parseInt(str_arr[j]);
+			
+			}
+
+		}
+		
+		
+		int max=Integer.MIN_VALUE;
+		StringBuilder ans=new StringBuilder("");
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res.length; j++) {
+
+				int min = arr[i][j];
+				if (i >= 1)
+					min = (min > arr[i - 1][j]) ? arr[i - 1][j] : min;
+				if (i >= 1 && j >= 1)
+					min = (min > arr[i - 1][j - 1]) ? arr[i - 1][j - 1] : min;
+				if (j >= 1)
+					min = (min > arr[i][j - 1]) ? arr[i][j - 1] : min;
+				if (i <= res.length - 2)
+					min = (min > arr[i + 1][j]) ? arr[i + 1][j] : min;
+				if (j <= res.length - 2)
+					min = (min > arr[i][j + 1]) ? arr[i][j + 1] : min;
+				if (i <= res.length - 2 && j <= res.length - 2)
+					min = (min > arr[i + 1][j + 1]) ? arr[i + 1][j + 1] : min;
+				if (i >= 1 && j <= res.length - 2)
+					min = (min > arr[i - 1][j + 1]) ? arr[i - 1][j + 1] : min;
+				if (j >= 1 && i <= res.length - 2)
+					min = (min > arr[i + 1][j - 1]) ? arr[i + 1][j - 1] : min;
+					
+				
+					max=(max<min)?min:max;
+					res[i][j]=min;
+			}
+
+		}
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res.length; j++) {
+				if(res[i][j]==max)
+					ans.append((i+1)+"#"+(j+1)+",");
+					
+				
+			}
+			
+		}
+
+		return ans.toString().split(",");
+	}
+	
+	
+	public static void main(String[] args) {
+		String[] inp2={"12#45#33","94#54#23","98#59#27"};
+		inp2=amount_value(3, inp2);
+		for (int i = 0; i < inp2.length; i++) {
+			System.out.println(inp2[i]);
+			
+		}
+	}
+}
--- Workspace/MyProj/src/hacker/rank/Calendar.java
+++ Workspace/MyProj/src/hacker/rank/Calendar.java
@@ -3,56 +3,55 @@ package hacker.rank;
 import java.util.Scanner;
 
 public class Calendar {
-	static String day[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};	
-	static String month[]={"January","February","March","April","May","June",
-		"July","August","September","October","November","December"};
+	static String day[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+	static String month[] = { "January", "February", "March", "April", "May",
+			"June", "July", "August", "September", "October", "November",
+			"December" };
 
-	static int noOfDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
+	static int noOfDays[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public static void leapYearCheck(int year) {
-		if((year%400==0)||(year%100!=0 && year%4==0))
-			noOfDays[1]=29;		
+		if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+			noOfDays[1] = 29;
 	}
 
-	public static int  getStartDay(int dd,int mm,int yyyy)
-	{
+	public static int getStartDay(int dd, int mm, int yyyy) {
 		int y = yyyy - (14 - mm) / 12;
-		int x = y + y/4 - y/100 + y/400;
+		int x = y + y / 4 - y / 100 + y / 400;
 		int m = mm + 12 * ((14 - mm) / 12) - 2;
-		int d = (dd + x + (31*m)/12) % 7;
+		int d = (dd + x + (31 * m) / 12) % 7;
 		return d;
 	}
 
-	public static int printMonth(int monthIndex,int startDay,int year)
-	{
-		System.out.format("%3s%s,%s\n\n"," ",month[monthIndex],year+"");
-		
-		for (int i = 0; i < day.length; i++) 
+	public static int printMonth(int monthIndex, int startDay, int year) {
+		System.out.format("%3s%s,%s\n\n", " ", month[monthIndex], year + "");
+
+		for (int i = 0; i < day.length; i++)
 			System.out.format("%6s", day[i]);
 
 		System.out.println("");
 
-		System.out.format("%"+((startDay>0)?(6*startDay):"")+"s"," ");
-		int dayCount=1;
-		for(;dayCount<=noOfDays[monthIndex];dayCount++)
-		{
-			if((dayCount+startDay)%7==1)
+		System.out.format("%" + ((startDay > 0) ? (6 * startDay) : "") + "s",
+				" ");
+		int dayCount = 1;
+		for (; dayCount <= noOfDays[monthIndex]; dayCount++) {
+			if ((dayCount + startDay) % 7 == 1)
 				System.out.println();
-		
-			System.out.format("%6s",dayCount+"");
+
+			System.out.format("%6s", dayCount + "");
 		}
 		System.out.println("\n");
-		return((dayCount+startDay-1)%7);
+		return ((dayCount + startDay - 1) % 7);
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the year:");
-		int year=scanner.nextInt();
+		int year = scanner.nextInt();
 		leapYearCheck(year);
-		int startDay=getStartDay(1,1,year);
-		for(int i=0;i<12;i++)
-			startDay= printMonth(i,startDay,year);
+		int startDay = getStartDay(1, 1, year);
+		for (int i = 0; i < 12; i++)
+			startDay = printMonth(i, startDay, year);
 		scanner.close();
 
 	}
