e47e687898643d6b754ea581ab37f0b937b7a3e1
Freeze777 <freezefrancis@gmail.com>
2016-10-05 23:26:24 +0530

    updated git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -3,4 +3,4 @@ git rm -r --cached Workspace/AlgoCodes/bin
 git rm -r --cached *.class
 git rm -r --cached *.o
 git commit -a
-git push git+ssh://git@github.com/Freeze777/mycodes.git
+git push https://github.com/Freeze777/mycodes.git
