e98fb1a671a85c11cda2981bdea94c963f909824
Freeze777 <freezefrancis@gmail.com>
2016-10-21 03:37:16 +0530

    removing crap

--- git_push.sh
+++ git_push.sh
@@ -3,5 +3,6 @@ git rm -r --cached Workspace/AlgoCodes/bin
 git rm -r --cached *.class
 git rm -r --cached *.o
 git rm -r --cached *.out
+git rm -r --cached *.*~
 git commit -a
 git push https://github.com/Freeze777/mycodes.git
