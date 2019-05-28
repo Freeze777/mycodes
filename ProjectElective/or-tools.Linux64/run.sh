572f83ec896b7d68fdcab751ed0123ad22d973a5
Freeze Francis <freezf@amazon.com>
2019-05-28 13:13:58 +0530

    Removing junk

--- ProjectElective/or-tools.Linux64/run.sh
+++
@@ -1,5 +0,0 @@
-
-javac -d objs -cp lib/com.google.ortools.jar examples/com/google/ortools/samples/$1.java
-
-java -Djava.library.path=lib -cp objs:lib/com.google.ortools.jar com.google.ortools.samples.$1
-
